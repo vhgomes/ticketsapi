e8952d09ec298441d442df26b467f6e47501a2ec
Victor Hugo <vhclaudiogomes@gmail.com>
2024-09-26 11:09:07 -0300

    git init

---
+++ src/main/java/vhgomes/com/ticketsti/TicketstiApplication.java
@@ -0,0 +1,13 @@
+package vhgomes.com.ticketsti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketstiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketstiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vhgomes/com/ticketsti/TicketstiApplicationTests.java
@@ -0,0 +1,13 @@
+package vhgomes.com.ticketsti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketstiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
